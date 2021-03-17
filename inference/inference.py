9cfe012740117c5245eb06a4348323d1f1a1b135
ireichard <ireichard4@gmail.com>
2021-03-17 11:00:18 -0700

    Added inference entry point

---
+++ inference/inference.py
@@ -0,0 +1,15 @@
+"""
+Inference System Entry Point
+Contributers:
+Ian Reichard
+
+"""
+
+
+def main():
+    """Main function"""
+    print("Hello from ian_dev branch!")
+
+
+if __name__ == "__main__":
+    main()
