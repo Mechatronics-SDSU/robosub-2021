d3a9c0cc9b6dbb832bb8bb76a6596e61e6367b71
ireichard <ireichard4@gmail.com>
2021-07-12 12:51:45 -0700

    Added a built script for Windows to rebuild the telemetry container. Renamed file to dockerfiles from Dockerfiles.

---
+++ tools/scripts/build_telemetry.bat
@@ -0,0 +1,9 @@
+@ECHO ON
+cd ..
+cd ..
+copy "%cd%\dockerfiles\Telemetry\Dockerfile" "%cd%\Dockerfile"
+copy "%cd%\dockerfiles\Telemetry\requirements.txt" "%cd%\requirements.txt"
+docker build -t telemetry_container .
+del Dockerfile
+del requirements.txt
+docker run -p 50003:50003 telemetry_container
