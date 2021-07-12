ca63dfbbe7b05d968c2db7fc0f721035ccc7b217
Christian Gould <christian.d.gould@gmail.com>
2021-07-12 00:49:41 -0700

    adding minimal positional changes

--- src/Intelligence/client.py
+++ src/Intelligence/client.py
@@ -1,3 +1,4 @@
+from multiprocessing import Process
 import os
 import buffer_pb2
 import buffer_pb2_grpc
@@ -6,49 +7,60 @@ import grpc
 import logging
 import cv2
 import pickle
-from inference_dir.gate_detector import GateDetector
-import server
-from multiprocessing import Process
+import docker
+import socket
+import subprocess
+
+from src.inference_dir.gate_detector import GateDetector
+
+CLIENT = docker.from_env()
+
+
+HOST = '127.0.0.1'
+PORT = 65432
 
 
 SERVER_ADDRESS = "localhost:23333"
+
+
 CLIENT_ID = 1
+start = False
+
 
 gate_detector = GateDetector()
 cap = cv2.VideoCapture('files/Additional_Test_Video.mp4')
 tracking = {}
-    
-def run():
-	with grpc.insecure_channel('localhost:50051') as channel:
-		try:
-			stub = buffer_pb2_grpc.Response_ServiceStub(channel)
-			i = 0
-			p = Process(target=process, args=(cap,stub,i,))
-			if p.is_alive() == False:
-				server.check(p, "start")
-			print("Is p alive? ", p.is_alive())
-			if False:
-				server.check(p, "kill")
-		except:
-			print('waiting for server to connect...')
-			time.sleep(1)
 
+class Spawn():
+	def __init__(self):	
+		self.run()
+		
+		
+	def run(self):
+		channel = grpc.insecure_channel('localhost:50051')
+		stub = buffer_pb2_grpc.Response_ServiceStub(channel)
+			#CLIENT.containers.run(name="client", command="sleep infinity", image="ubuntu:latest", detach=True)
+		response_string = b"start"
+		stub.Info(buffer_pb2.Send_Request(send=response_string))
+		term_socket()
+				
 
-def process(cap,stub,i):
-	while True:
-		_, _frame = cap.read()
-		result = gate_detector.detect(_frame)
-		if result != None:
-			i += 1
-			val = pickle.dumps(result)
-			response = stub.Info(buffer_pb2.Send_Request(send=val))
-			tracking[i] = response.message
-			print(response.message)
-
-
+def term_socket():
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		s.bind((HOST, PORT))
+		s.listen(5)
+		conn, addr = s.accept()
+		while True:
+			data = conn.recvfrom(1024)[0]
+			print(data)
+		conn.close()
+			
+			
 def stop():
-	return "Not all good"			
+	server.check(p, "stop")
+				
    
 if __name__ == '__main__':
 	logging.basicConfig()
-	run()
+	Spawn()
--- src/Intelligence/server.py
+++ src/Intelligence/server.py
@@ -1,4 +1,6 @@
 from concurrent import futures
+from multiprocessing import Process
+
 import grpc
 import buffer_pb2
 import buffer_pb2_grpc
@@ -7,25 +9,58 @@ import threading
 import logging
 import pickle
 import os
-import client
+import cv2
+
+
+import socket
+import docker
+import subprocess
+
+from src.inference_dir.gate_detector import GateDetector
+
+CLIENT = docker.from_env()
+
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+
+SERVER_ADDRESS = "localhost:23333"
+
+
+gate_detector = GateDetector()
+cap = cv2.VideoCapture('files/Additional_Test_Video.mp4')
 
 
 class Listener(buffer_pb2_grpc.Response_ServiceServicer):
+	def __init__(self):
+		buffer_pb2_grpc.Response_ServiceServicer.__init__(self)
+		self.p = None
+
+
 	def Info(self, request, context):
-		retriever = bytes(request.send)
-		val = pickle.loads(retriever)
-		print(val)
-		return buffer_pb2.Request_Response(message = bytes('Bytes Recieved: %s!' % request.send, 'utf-8'))
+			#CLIENT.containers.run(name="server", command="sleep infinity", image="ubuntu:latest", detach=True)
+		retriever = request.send
+		decode = retriever.decode("utf-8")
+		if  decode == "start":
+			self.p = Process(target=process, args=(cap,))
+			self.p.start()
+			return buffer_pb2.Request_Response(message = bytes('ok', 'utf-8'))
+		if request.send == "stop":
+			self.p.kill()
+			return buffer_pb2.Request_Response(message = bytes('ok', 'utf-8'))
 
 
-def check(p, cmd):
-	if cmd == "start":
-		print("Process is good to start")
-		p.start()
-	if cmd == "kill":
-		p.kill()
-	if cmd == "check":
-		print("Is p alive? ", p.is_alive())
+def process(cap,):
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+			s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			s.connect((HOST,PORT))
+			while True:
+				_, _frame = cap.read()
+				result = gate_detector.detect(_frame)
+				val = pickle.dumps(result)
+				s.sendall(val)
+		
 			
 def serve():
 	server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
