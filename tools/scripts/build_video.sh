7d50deae2ffa065d4400310b691fc15e5d52a2b4
ireichard <ireichard4@gmail.com>
2021-07-15 23:34:46 -0700

    Added and tested build script for Video container and tested all containers concurrently on Pi

---
+++ tools/scripts/build_video.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Setting up Video Container"
+cd ..
+cd ..
+cp $(pwd)/dockerfiles/Video/Dockerfile $(pwd)/Dockerfile
+cp $(pwd)/dockerfiles/Video/requirements.txt $(pwd)/requirements.txt
+docker build . -t video_container
+rm Dockerfile
+rm requirements.txt
+echo "Setup Complete."
+docker run --name inf_video --device /dev/video0:/dev/video0  --publish 50001:50001 video_container
