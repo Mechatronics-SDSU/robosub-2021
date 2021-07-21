55b78f5ee2181cf3db16989e0012958978e42962
ireichard <ireichard4@gmail.com>
2021-07-20 21:09:57 -0700

    Fix for missing double quotes in dirpath causing word splitting

--- tools/scripts/build_logging.sh
+++ tools/scripts/build_logging.sh
@@ -2,8 +2,8 @@
 echo "Setting up Logging Container"
 cd ..
 cd ..
-cp $(pwd)/dockerfiles/Logging/Dockerfile $(pwd)/Dockerfile
-cp $(pwd)/dockerfiles/Logging/requirements.txt $(pwd)/requirements.txt
+cp "$(pwd)/dockerfiles/Logging/Dockerfile" "$(pwd)/Dockerfile"
+cp "$(pwd)/dockerfiles/Logging/requirements.txt" "$(pwd)/requirements.txt"
 docker build . -t logging_container
 rm Dockerfile
 rm requirements.txt
