4e695b75bccb8cf62f5296f4f8f225031e3f746a
Jimmy Musser <jmusser@sdsu.edu>
2021-07-22 13:29:05 -0700

    Fixes #27

--- src/Intelligence/Client_Docker/runclient.sh
+++ src/Intelligence/Client_Docker/runclient.sh
@@ -10,7 +10,7 @@ cp "$(pwd)/src/Intelligence/Client_Docker/Dockerfile" "$(pwd)/Dockerfile"
 docker build . -t client_container
 #del Dockerfile
 
-docker run -dit --name client --publish 50051:50051 --network=sc-arch client_container 
+docker run -dit --name client --publish 50051:50051 --network=host client_container 
 
 docker container attach client
 
--- src/Intelligence/Server_Docker/runserver.sh
+++ src/Intelligence/Server_Docker/runserver.sh
@@ -13,6 +13,6 @@ docker build . -t server_container
 
 docker network create sc-arch
 
-docker run -dit --name server --publish 65432:65432 --network=sc-arch server_container 
+docker run -dit --name server --publish 65432:65432 --network=host server_container 
 
 docker container attach server
